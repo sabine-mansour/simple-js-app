60ad3b6c4637c2fef79a4ca7a3b18ccf5952bfef
sabine-mansour <sabine-mansour@users.noreply.github.com>
2021-02-28 12:17:09 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let myName = 'Sabine';
+document.write(myName);
+myName = 'John';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
