f75930925edb461f26029beb38338874c68894d0
sabine-mansour <sabine-mansour@users.noreply.github.com>
2021-03-01 15:58:34 +0100

    created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World');
-
-let favoriteFood = 'Lasagna';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Spearow', height: 3, types: ['Flying']},
+  { name: 'Bulbasaur', height: 7, types: ['Monster', 'Grass']},
+  { name: 'Squirtle', height: 5, types: ['Monster', 'Water']}
+];
