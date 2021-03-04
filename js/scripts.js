f53bd8a8708403ef392a5895d70fc45f06b1a342
sabine-mansour <sabine-mansour@users.noreply.github.com>
2021-03-04 10:06:27 +0100

    Added loop with conditional to the pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ let pokemonList = [
   { name: 'Bulbasaur', height: 7, types: ['Monster', 'Grass']},
   { name: 'Squirtle', height: 5, types: ['Monster', 'Water']}
 ];
+
+//Loop iterates each pokemon name  and height.
+for (let i=0; i < pokemonList.length; i++){
+  //Conditional checks if a pokemon'height is above average.
+  if (pokemonList[i].height > 5){
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +") - Wow, that’s big!<br>")
+  } else {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +")<br>")
+  }
+}
