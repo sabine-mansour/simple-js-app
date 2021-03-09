8e5f26f3cafe9c8231bd43b093acd250437b06b1
sabine-mansour <sabine-mansour@users.noreply.github.com>
2021-03-09 16:35:30 +0100

    Wrapped pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
-let pokemonList = [
-  { name: 'Spearow', height: 3, types: ['Flying']},
-  { name: 'Bulbasaur', height: 7, types: ['Monster', 'Grass']},
-  { name: 'Squirtle', height: 5, types: ['Monster', 'Water']}
-];
+let pokemonRepository = (function(){
+  let pokemonList = [
+    { name: 'Spearow', height: 3, types: ['Flying']},
+    { name: 'Bulbasaur', height: 7, types: ['Monster', 'Grass']},
+    { name: 'Squirtle', height: 5, types: ['Monster', 'Water']}
+  ];
 
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(item){
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
 
 //forEach Loop iterates each pokemon name  and height.
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   //Conditional checks if a pokemon'height is above average.
   if (pokemon.height > 5) {
     document.write(pokemon.name + " (height: " + pokemon.height +") - Wow, that’s big!<br>" )
