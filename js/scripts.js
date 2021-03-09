66213999c868ed3011b8c3010c1f10ea5312c654
sabine-mansour <sabine-mansour@users.noreply.github.com>
2021-03-09 15:47:15 +0100

    Replaced for loop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,13 @@ let pokemonList = [
   { name: 'Squirtle', height: 5, types: ['Monster', 'Water']}
 ];
 
-//Loop iterates each pokemon name  and height.
-for (let i=0; i < pokemonList.length; i++){
+
+//forEach Loop iterates each pokemon name  and height.
+pokemonList.forEach(function(pokemon) {
   //Conditional checks if a pokemon'height is above average.
-  if (pokemonList[i].height > 5){
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +") - Wow, that’s big!<br>")
-  } else {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +")<br>")
+  if (pokemon.height > 5) {
+    document.write(pokemon.name + " (height: " + pokemon.height +") - Wow, that’s big!<br>" )
+  }else{
+    document.write(pokemon.name + " (height: " + pokemon.height +")<br>")
   }
-}
+});
